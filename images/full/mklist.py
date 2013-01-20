09eeb655545a94c683b02c0c2d4512a6e123d1a3
David Ascher <david.ascher@gmail.com>
2013-01-19 19:17:24 -0800

    + ‘images/full/blah.thml’ / ‘images/full/mklist.py’

--- images/full/mklist.py
+++ images/full/mklist.py
@@ -1,5 +1,5 @@
 # mklist.py
 
-for line in open('list').split():
+for line in open('list').readlines():
   img = line.strip()
   print '"<li><a href="images/full/' + img + '"><img src="images/thumb/' + img + '" alt="' + img + '" /></a></li>'

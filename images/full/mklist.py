62485ac6ff56446dbb84068a698741222fc65803
David Ascher <david.ascher@gmail.com>
2013-01-19 19:42:25 -0800

    / ‘images/full/mklist.py’

--- images/full/mklist.py
+++ images/full/mklist.py
@@ -2,4 +2,4 @@
 
 for line in open('list').readlines():
   img = line.strip()
-  print '<li><a href="images/full/' + img + '"><img src="images/thumb/' + img + '" alt="' + img + '" /></a></li>'
+  print '<li class="nonpick"><a href="images/full/' + img + '"><img src="images/thumb/' + img + '" alt="' + img + '" /></a></li>'
